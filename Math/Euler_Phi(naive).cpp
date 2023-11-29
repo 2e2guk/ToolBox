f43cc0f84c3e454d2d231d5ace05425a4b77aab1
이강욱 <90788409+2e2guk@users.noreply.github.com>
2023-11-29 19:41:36 +0900

    Create Euler_Phi(naive).cpp

---
+++ Math/Euler_Phi(naive).cpp
@@ -0,0 +1,34 @@
+// 오일러 피 함수(naive)
+// O(sqrt(N))
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+template<typename T>
+T eulerPhi(T n) {
+    T ans = n;
+    T num = n;
+    for (T i = 2; i * i <= n; ++i) {
+        if (num % i == 0) {
+            ans /= i;
+            ans *= i - 1;
+            while (num % i == 0) {
+                num /= i;
+            }
+        }
+    }
+
+    if (num != 1) {
+        ans /= num;
+        ans *= num - 1;
+    }
+    return ans;
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    ll n; cin >> n;
+    cout << eulerPhi(n);
+    return 0;
+}
