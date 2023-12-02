1f067b6ae2a910ea98d423b2eb41c9cdcc51988b
이강욱 <90788409+2e2guk@users.noreply.github.com>
2023-12-02 22:40:10 +0900

    Update Rotating_Calipers.cpp

--- Geometry/Rotating_Calipers.cpp
+++ Geometry/Rotating_Calipers.cpp
@@ -10,14 +10,14 @@ template<typename PointType>
 struct Point {
     PointType x, y;
 
-    Point operator + (const Point& p) const { return {x + p.x, y + p.y}; }
-    Point operator - (const Point& p) const { return {x - p.x, y - p.y}; }
-    PointType operator * (const Point& p) const { return x * p.x + y * p.y; } // 내적
-    PointType operator / (const Point& p) const { return x * p.y - p.x * y; } // 외적
+    Point<PointType> operator + (const Point<PointType>& p) const { return {x + p.x, y + p.y}; }
+    Point<PointType> operator - (const Point<PointType>& p) const { return {x - p.x, y - p.y}; }
+    PointType operator * (const Point<PointType>& p) const { return x * p.x + y * p.y; } // 내적
+    PointType operator / (const Point<PointType>& p) const { return x * p.y - p.x * y; } // 외적
 
-    static PointType Dist(const Point& p1, const Point& p2) { return (p2 - p1) * (p2 - p1); }
-    static PointType SignedArea(const Point& p1, const Point& p2, const Point& p3) { return (p2 - p1) / (p3 - p1); }
-    static PointType CCW(const Point& p1, const Point& p2, const Point& p3) { 
+    static PointType Dist(const Point<PointType>& p1, const Point<PointType>& p2) { return (p2 - p1) * (p2 - p1); }
+    static PointType SignedArea(const Point<PointType>& p1, const Point<PointType>& p2, const Point<PointType>& p3) { return (p2 - p1) / (p3 - p1); }
+    static PointType CCW(const Point<PointType>& p1, const Point<PointType>& p2, const Point<PointType>& p3) { 
         PointType area = SignedArea(p1, p2, p3);
         return (area > 0) - (area < 0);
     }
