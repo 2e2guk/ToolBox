28556b3c825831d79c549eaf01603b82b4e898c6
이강욱 <90788409+2e2guk@users.noreply.github.com>
2024-04-02 00:57:06 +0900

    Update MergeSortTree.cpp

--- query처리/MergeSortTree.cpp
+++ query처리/MergeSortTree.cpp
@@ -8,25 +8,25 @@ const int MAX_ST = 1 << 18;
  
 // 머지 소트 트리
 template<typename ValueType>
-struct MergesortTree{
+struct MergesortTree {
     vector<ValueType> arr[MAX_ST];
 
-    void construct(){
-        for(int i = MAX_ST/2-1; i > 0; --i){
+    void construct() {
+        for(int i = MAX_ST/2-1; i > 0; i--) {
             vector<ValueType> &c = arr[i], &l = arr[i*2], &r = arr[i*2+1];
             arr[i].resize(l.size()+r.size());
-            for(int j = 0, p = 0, q = 0; j < c.size(); ++j){
+            for(int j = 0, p = 0, q = 0; j < c.size(); ++j) {
                 if(q == r.size() || (p < l.size() && l[p] < r[q])) c[j] = l[p++];
                 else c[j] = r[q++];
             }
         }
     }
  
-    int greater(int s, int e, int k, int node = 1, int ns = 0, int ne = MAX_ST / 2){
+    int greater(int s, int e, int k, int node = 1, int ns = 0, int ne = MAX_ST / 2) {
         if(ne <= s || e <= ns) return 0;
         if(s <= ns && ne <= e)
             return arr[node].end() - upper_bound(arr[node].begin(), arr[node].end(), k);
-        int mid = (ns + ne)/2;
+        int mid = (ns + ne) / 2;
         return greater(s, e, k, node * 2, ns, mid) + greater(s, e, k, node * 2 + 1, mid, ne);
     }
 };
@@ -38,7 +38,7 @@ int main() {
     MergesortTree<ll> ST;
 
     cin >> N;
-    for(int i = 0; i < N; ++i) {
+    for(int i = 0; i < N; i++) {
         ll val; cin >> val;
         ST.arr[MAX_ST / 2 + i].push_back(val);
     }
@@ -46,7 +46,7 @@ int main() {
     ST.construct();
 
     cin >> M;
-    for(int i = 0; i < M; ++i) {
+    for(int i = 0; i < M; i++) {
         ll s, e, k; cin >> s >> e >> k;
         cout << (L = ST.greater((s^L) - 1, e^L, k^L)) << "\n";
     }
