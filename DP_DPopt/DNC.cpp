ebfe7e277e37d18b515a3d1aca360be037fabdcd
김상현(KssssssH) <57479300+rlatkdgus2627@users.noreply.github.com>
2024-10-13 00:03:17 +0900

    fix: Update DNC.cpp
    DNC.cpp 내용 일부 수정

--- DP_DPopt/DNC.cpp
+++ DP_DPopt/DNC.cpp
@@ -1,4 +1,4 @@
-//dp[t][i]=min{k<i}(dp[t?1][k]+C[k][i])
+//dp[t][i]=min{k<i}(dp[t-1][k]+C[k][i])
 //K[t][i]는 dp[t][i]를 만족시키는 최소 k라 할 때 다음 부등식을 만족 K[t][i]≤K[t][i+1]
 //혹은 비용 C에 대해 C[a][c]+C[b][d]<=C[a][d]+C[b][c]
 #include <bits/stdc++.h>
@@ -8,16 +8,15 @@ using ll = long long;
 
 const ll INF = 1e18;
 int L, G, K[801][8001];
-
 ll psum[8001], C[801][1001];
 ll dp[801][8001], ans;
 //dncOpt 기본형, dp[mid] < (i - mid) * t[i] + v[mid]처럼 t가 없는 경우도 가능
 void dncOptOriginal(int t, int s, int e, int optS, int optE){ 
-    // D[t][s], D[t][s+1], ..., D[t][e]를 계산하는 함수. [optS, optE] 가능한 k의 범위
+    // dp[t][s], dp[t][s+1], ..., dp[t][e]를 계산하는 함수. [optS, optE] 가능한 k의 범위
     if(s > e) return;
         int mid = s + e>> 1;
         dp[t][mid] = 1e18;
-        for (int k = optS; k <= optE && k < mid; k++){// D[t][m]에 대해 가능한 답을 선형 시간에 계산
+        for (int k = optS; k <= optE && k < mid; k++){// dp[t][m]에 대해 가능한 답을 선형 시간에 계산
             ll value = dp[t-1][k] + C[k][mid];
             if (dp[t][mid] > value)
                 dp[t][mid] = value, K[t][mid] = k;
@@ -28,9 +27,8 @@ void dncOptOriginal(int t, int s, int e, int optS, int optE){
         dncOptOriginal(t, mid + 1, e, K[t][mid], optE);
         // 2, 3에 대해 가능한 k의 후보는 조건에 의해 줄어든다.
 }
-
+//13261 풀이
 void dncOptEx(int t, int s, int e, int optS, int optE){
-    //13261 풀이
 	if(s > e)	return;
 	int mid = s + e >> 1;
 	int opt = 0;
