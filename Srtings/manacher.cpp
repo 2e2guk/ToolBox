a0e5d4bb0820a6bf851bb5f80e7c87feb66ff1ae
김상현(KssssssH) <57479300+rlatkdgus2627@users.noreply.github.com>
2024-10-13 01:37:39 +0900

    fix: Update manacher.cpp
    소스코드 인덴트 수정

--- Srtings/manacher.cpp
+++ Srtings/manacher.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 
 int manacher(string inputStr){
-    int strLen = inputStr.size(), r = 0, p = 0, ret = 0;
-    string str = " ";
+	int strLen = inputStr.size(), r = 0, p = 0, ret = 0;
+	string str = " ";
 	for(int i = 0; i < strLen; i++){
 		str += inputStr[i];
 		str += " ";
 	}
-    strLen = str.size();
-    vector<int> A(strLen + 1);
-    for(int i = 0; i < strLen; i++){
+	strLen = str.size();
+	vector<int> A(strLen + 1);
+	for(int i = 0; i < strLen; i++){
 		if(i <= r)	A[i] = min(r - i, A[2 * p - i]);
 		while(0 <= i - A[i] - 1 && i + A[i] + 1 < strLen && str[i - A[i] - 1] == str[i + A[i] + 1]) A[i]++;
 		if(r < i + A[i]){
@@ -21,18 +21,18 @@ int manacher(string inputStr){
 		}
 	}
 
-    for(int i = 1; i <= strLen; i++){
+	for(int i = 1; i <= strLen; i++){
 		ret = max(ret, A[i]);
 	}
-
-    return ret;
+	
+	return ret;
 }
 
 int main(){
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
-    string input;
-    cin >> input;
-    cout << manacher(input);
-    return 0;
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	string input;
+	cin >> input;
+	cout << manacher(input);
+	return 0;
 }
